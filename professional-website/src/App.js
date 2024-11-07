eb8aff8e6787f910b5c5e59bd3fa199b5d545e81
AlecOverman <aover003@ucr.ecu>
2024-11-06 20:06:29 -0800

    fixing the display flex and sections

--- professional-website/src/App.js
+++ professional-website/src/App.js
@@ -1,16 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from "./components/Navbar.js";
+import HomePage from "./components/HomePage.js";
 
 function App() {
   return (
     <div className="App">
-      <Navbar className="navbar">
-        <section id="home" className="navbar-section"><h2>Home</h2>
-          <p>Welcome to my professional website!</p>
-        </section>
-        
-      </Navbar>
+      <Navbar className="navbar"></Navbar>
+      <section id="home" className="navbar-section">
+          <HomePage></HomePage>
+      </section>
+      <p>Coming Soon! (Updated Nov 6)</p>
+
     </div>
   );
 }
---
+++ professional-website/src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./HomePage.css";
+
+function Homepage() {
+    return(
+        <div className="home-body"></div>
+
+    );
+
+}
+
+export default Homepage;
